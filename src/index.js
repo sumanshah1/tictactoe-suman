7bbfd180869b536a2906be454cee37503506a03b
sumanshah1 <shahsuman2076@gmail.com>
2022-09-13 23:39:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+
+ReactDom.render(
+  <>
+  <h1 style={{textAlign:"center"}
+ } >Chaliya Suru karte hai!!</h1>
+  <App/>
+
+  </>,document.getElementById("root"));
